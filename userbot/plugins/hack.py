2bbf99dbc860a98c3be84405c02e51b6b886ab85
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2020-02-05 02:31:28 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -43,7 +43,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 69$ To` @Zains_Tech `Or send just 3 BTC`"
+            "`Targeted Account Hacked...\n\nPay 69$ To` @Zains_Tech `Or send just 3 BTC to remove this hack`"
         ]
 
         for i in animation_ttl:
